6ca3c52853694b88a9188e46aa7e7a0883999356
SebastianBer <jesussebastianbber@ufps.edu.co>
2023-07-27 11:25:20 -0500

    Final

---
+++ src/CounterApp.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+export const CounterApp = ({value}) =>{
+    
+    const [ counter, setCounter ] = useState(value);
+
+    const handleAdd = () =>{
+        // setCounter(counter + 1);
+        setCounter((c)=> c +1);
+    }
+
+    const resta = () => setCounter(counter - 1);
+    
+
+    const reset = () => setCounter(value);
+    
+
+    return(
+        <>
+        <h1> CounterApp </h1>
+        <h2> { counter } </h2>
+        <button onClick={ handleAdd }> +1 </button>
+        <button onClick={resta}> -1 </button>
+        <button onClick={reset}> Reset </button>
+        </>
+)}
+
+CounterApp.protoTypes  = {
+    value: PropTypes.number.isRequired
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 //Para renderizar
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './HelloWorldApp'
-import { FirstApp } from './FirstApp';
+// import {App} from './HelloWorldApp'
+// import { FirstApp } from './FirstApp';
 import './styles.css'
+import { CounterApp } from './CounterApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp />
+        <CounterApp value= {20} />
     </React.StrictMode>
 );
